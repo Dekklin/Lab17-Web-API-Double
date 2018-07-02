bf556e4a735f121490aa2712b989ac67585c8e6a
Collin Hintzke <collinhintzke5@gmail.com>
2018-07-01 21:42:33 -0700

    Moved files

--- Lab17-WebApi/Controllers/ToDosController.cs
+++ Lab17-WebApi/Controllers/ToDosController.cs
@@ -14,18 +14,24 @@ namespace Lab17_WebApi.Controllers
     public class ToDosController : ControllerBase
     {
         private readonly ToDoContext _context;
-
+        /// <summary>
+        /// Creates a private reference to the database and creates a blank ToDo if none exist
+        /// </summary>
+        /// <param name="context"></param>
         public ToDosController(ToDoContext context)
         {
             _context = context;
             if(_context.ToDos.Count() == 0)
             {
-                _context.ToDos.AddAsync(new ToDo { Title = "Item1" });
-                _context.SaveChangesAsync();
+                _context.ToDos.Add(new ToDo { Title = "Item1" });
+                _context.SaveChanges();
             }
         }
 
-        // GET: api/ToDos
+        /// <summary>
+        /// Retrieves all the ToDo items
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<List<ToDo>> GetAll()
         {
@@ -33,10 +39,10 @@ namespace Lab17_WebApi.Controllers
         }
 
         // GET: api/ToDos/5
-        [HttpGet("{id}:int")]
-        public ActionResult<ToDo> GetById(long id)
+        [HttpGet("{id}", Name = "GetToDo")]
+        public async Task<ActionResult<ToDo>> GetById(int id)
         {
-            var item = _context.ToDos.Find(id);
+            var item = await _context.ToDos.FindAsync(id);
             if(item == null)
             {
                 return NotFound();
@@ -47,17 +53,18 @@ namespace Lab17_WebApi.Controllers
 
         // POST: api/ToDos
         [HttpPost]
-        public IActionResult Create([FromBody]ToDo item)
+        public async Task<IActionResult> Create(ToDo item)
         {
-            _context.ToDos.AddAsync(item);
-            _context.SaveChangesAsync();
+            await _context.ToDos.AddAsync(item);
+            await _context.SaveChangesAsync();
             return CreatedAtRoute("GetTodo", new { id = item.ID }, item);
         }
 
         // PUT: api/ToDos/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id,[FromBody] ToDo item)
+        public async Task<IActionResult> Update([FromRoute]int id,[FromBody] ToDo item)
         {
+            item.ID = id;
             if (id != item.ID)
                 return BadRequest();
             var todo = _context.ToDos.Find(id);
@@ -67,14 +74,14 @@ namespace Lab17_WebApi.Controllers
             }
             todo.Finished = item.Finished;
             todo.Title = item.Title;
-            await _context.ToDos.AddAsync(todo);
+            _context.ToDos.Update(todo);
             await _context.SaveChangesAsync();
             return NoContent();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public IActionResult Delete(long id)
+        public IActionResult Delete(int id)
         {
             var todo = _context.ToDos.Find(id);
             if(todo == null)
